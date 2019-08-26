dcf62223873d9d138798126a956c4fb5574dc2b1
NikolayPautov <nikolaypautov.sitecore@gmail.com>
2019-08-26 14:13:52 +0300

    Initial code

---
+++ src/Sitecore.Support.353799/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.353799")]
+[assembly: AssemblyProduct("Sitecore.Support.353799")]
+[assembly: ComVisible(false)]
---
+++ src/Sitecore.Support.353799/Shell/Applications/Security/RoleManager/NewRolePage/NewRolePage.cs
@@ -0,0 +1,31 @@
+﻿using Sitecore.Security;
+using Sitecore.StringExtensions;
+using Sitecore.Web.UI.Sheer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sitecore.Support.Shell.Applications.Security.RoleManager.NewRolePage
+{
+    public class NewRolePage: Sitecore.Shell.Applications.Security.RoleManager.NewRolePage
+    {
+        protected override void OK_Click()
+        {
+            string text = this.Name.Text;
+            if (!SecurityUtil.IsValidRoleName(ref text))
+            {
+                string[] arguments = new string[] { text };
+                SheerResponse.Alert("The role name \"{0}\" contains illegal characters.\n\nThe role name can only contain the following characters: A-Z, a-z, 0-9, ampersand and space.", arguments);
+            }
+            else
+            {
+                object[] parameters = new object[] { this.Domain.SelectedValue, text };
+                SheerResponse.SetDialogValue(@"{0}\{1}".FormatWith(parameters));
+                base.OK_Click();
+            }
+        }
+
+
+    }
+}
